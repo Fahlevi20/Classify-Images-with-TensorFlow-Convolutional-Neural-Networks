0f2ce6484fb0501c3c64c9b274eb62091bd7e250
Mukhammad Fahlevi Ali Rafsanjani <64582353+Fahlevi20@users.noreply.github.com>
2024-05-21 19:47:57 +0700

    Update Classify-Images-with-TensorFlow-Convolutional-Neural-Networks.py

--- Classify-Images-with-TensorFlow-Convolutional-Neural-Networks.py
+++ Classify-Images-with-TensorFlow-Convolutional-Neural-Networks.py
@@ -2,3 +2,12 @@
 !pip install --user tensorflow-text
 !pip install --user tensorflow-datasets
 !pip install protobuf==3.20.1
+
+import os
+
+if not os.getenv("IS_TESTING"):
+    # Automatically restart kernel after installs
+    import IPython
+
+    app = IPython.Application.instance()
+    app.kernel.do_shutdown(True)
